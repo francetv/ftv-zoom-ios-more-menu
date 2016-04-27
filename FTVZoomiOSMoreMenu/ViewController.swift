f659f1027e83b01676efa7ad1242f5cd3db0d8c5
William Archimede <william.archimede@hoodbrains.com>
2016-04-27 18:07:50 +0200

    Refactored the view controller

--- FTVZoomiOSMoreMenu/ViewController.swift
+++ FTVZoomiOSMoreMenu/ViewController.swift
@@ -10,13 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(animated)
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
 
         addContentView()
     }
